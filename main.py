a35466bd47931873ad4689d668ed23288576d616
SileboxUnderfined <valerkafm@gmail.com>
2022-11-11 00:50:28 +0300

    final

--- main.py
+++ main.py
@@ -1,11 +1,127 @@
 from telebot.async_telebot import AsyncTeleBot
 from os import environ as envv
-import asyncio
+import asyncio, aiosqlite, signal, sys
+from random import randint
+from datetime import datetime, timedelta
+from quickchart import QuickChart
 
 bot = AsyncTeleBot(envv['BOT_TOKEN'])
+timeFormat = "%Y%m%d%H%M%S"
+
+def exit_handler(sig, frame):
+    print('exiting...')
+    sys.exit(0)
+
+async def genrand():
+    result = 0
+    while result == 0: result = randint(-9,9)
+    return result
+
+async def getNextTime():
+    r = datetime.now() + timedelta(hours=12)
+    return r.strftime(timeFormat)
+
+async def getDateTime(s):
+    return datetime.strptime(s,timeFormat)
+
+def topSort(x):
+    return x[1]
+
+async def getSorted(data):
+    return list(reversed(sorted(data, key=topSort)))
+
+async def getUserInfo(chat_id,user_id):
+    return await bot.get_chat_member(chat_id, user_id)
+
+signal.signal(signal.SIGINT, exit_handler)
 
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
     await bot.reply_to(message,"""привет дурак дебильный ебальный\nблагодаря этому боту ты можешь меряться письками с другими дебилами\nчтобы использовать эту бесконечно великую функцию, введи /dick\nвведи /help """)
+    print('/start command for', message.from_user)
+
+@bot.message_handler(commands=['dick'])
+async def dick_func(message):
+    new_dick = await genrand()
+    next_time = await getNextTime()
+    user_id = int(message.from_user.id)
+    chat_id = int(message.chat.id)
+    async with aiosqlite.connect("data.db") as db:
+        async with db.execute(f"SELECT * FROM users WHERE user_id = {user_id} AND chat_id = {chat_id}") as cursor:
+            data = await cursor.fetchall()
+            print(data)
+            if len(data) == 0:
+                print([element for element in data])
+                await db.execute(f"INSERT INTO users (user_id, dick_length, next_date, chat_id) VALUES ({user_id},{new_dick}, {next_time}, {message.chat.id})")
+                await db.commit()
+                await bot.reply_to(message, f"Тебе создали новый хуй, поздравляю. Он равен {new_dick} см.")
+            else:
+                async with db.execute(f"SELECT next_date FROM users WHERE user_id = {user_id} AND chat_id={chat_id}") as ct:
+                    next_timeU = await ct.fetchone()
+                    next_timeU = await getDateTime(next_timeU[0])
+
+                nowDate = datetime.now()
+                delta = next_timeU - nowDate
+                r = str(timedelta(seconds=delta.seconds)).split(":")
+                if nowDate < next_timeU:
+                    await bot.reply_to(message, f"Ты идиот. Жди ещё {r[0]} часов, {r[1]} минут и {r[2]} секунд")
+                else:
+                    now_dick = [user[2] for user in data if user[1] == user_id][0]
+                    await db.execute(f"UPDATE users SET dick_length = {new_dick + now_dick} WHERE user_id = {user_id} AND chat_id={chat_id}")
+                    await db.execute(f"UPDATE users SET next_date = {next_time} WHERE user_id = {user_id} AND chat_id={chat_id}")
+                    await db.commit()
+                    await bot.reply_to(message, f"Твой хер был обновлён на {new_dick}, поздравляю. Сейчас он равен {new_dick + now_dick}")
+    print("/dick command for ", user_id)
+
+@bot.message_handler(commands=["top"])
+async def top_func(message):
+    chat_id = message.chat.id
+    async with aiosqlite.connect("data.db") as db:
+        async with db.execute(f"SELECT user_id, dick_length FROM users WHERE chat_id = {chat_id}") as cursor:
+            data = await cursor.fetchall()
+            if len(data) == 0: await bot.reply_to(message,"вы пока не призывали письки")
+            else:
+                data = await getSorted(data)
+
+                result = f"Топ лучших:\n"
+                for user in range(len(data)):
+                    user_info = await getUserInfo(chat_id, data[user][0])
+                    print(user_info)
+                    result += f"{user+1}: {user_info.user.username} с хуём в размере {data[user][1]} см\n"
+                await bot.reply_to(message,result)
+
+@bot.message_handler(commands=["graph"])
+async def graph_func(message):
+    chat_id = message.chat.id
+    async with aiosqlite.connect("data.db") as db:
+        async with db.execute(f"SELECT user_id, dick_length FROM users WHERE chat_id = {chat_id}") as cursor:
+            data = await cursor.fetchall()
+            if len(data) == 0: await bot.reply_to(message,"вы пока не призывали письки")
+            else:
+                data = await getSorted(data)
+                index = data[0][1]
+                dfg = {}
+                for user in data:
+                    user_info = await getUserInfo(chat_id,user[0])
+                    dfg.update({user_info.user.username:user[1]})
+
+                qc = QuickChart()
+                qc.width = 500
+                qc.height = 300
+                qc.config = {
+                    "type":"pie",
+                    "data": {
+                        "labels":[key for key in dfg.keys()],
+                        "datasets":[
+                            {
+                                "data":[value for value in dfg.values()]
+                            }
+                        ]
+                    }
+                }
+                url = qc.get_url()
+                await bot.send_photo(chat_id,url)
+
+
 
 asyncio.run(bot.polling())
